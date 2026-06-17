8b86d29f094e574a5b1900b9e47d767b65ec299c
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-18 06:01:21 +0700

    feat(ui): load acceptance-aligned responsive styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,6 +10,7 @@ import "./clean-layout.css";
 import "./workflow-pages.css";
 import "./bon-form.css";
 import "./final-template.css";
+import "./acceptance-ui.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
