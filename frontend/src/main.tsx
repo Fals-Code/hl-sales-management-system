97bc66be9d72eace425385a99747165711bd6b08
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-18 04:55:48 +0700

    feat(ui): load final clean mobile-first styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import "./login.css";
 import "./polish.css";
 import "./responsive.css";
 import "./summary-cards.css";
+import "./clean-layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
