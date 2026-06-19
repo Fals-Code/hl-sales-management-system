49c14e8f189d5001a3bafefc679a8ac689621913
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-19 09:34:16 +0700

    refactor(ui): load final visual hierarchy refinement

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,6 +17,7 @@ import "./sidebar-refinement.css";
 import "./navbar-refinement.css";
 import "./profile-menu-refinement.css";
 import "./bon-dialog-layout-fix.css";
+import "./visual-hierarchy-refinement.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
