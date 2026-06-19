447c630aef5c9eb64d40a280a566bc99dc13d354
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-20 01:05:05 +0700

    style(ui): load POS page refinements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -26,6 +26,7 @@ import "./report-preview-refinement.css";
 import "./phase4-audit.css";
 import "./phase5-readiness.css";
 import "./bon-create-page.css";
+import "./pos-page-extras.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
